fdcb800ee984e18b17021bd32695b55d484e5e6b
Florian Bénitière <flben@narval1.narval.calcul.quebec>
2025-07-13 15:45:47 -0400

    update

--- setup/ccdb/nextflow-run.sh
+++ setup/ccdb/nextflow-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=SPARK_XHMM_DB    # Job name
+#SBATCH --job-name=cnvDB_Buider    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=3500MB                     # Job memory request
-#SBATCH --time= 01:00:00               # Time limit hrs:min:sec
+#SBATCH --time=01:00:00               # Time limit hrs:min:sec
 #SBATCH --output=SPARK_XHMM_DB_%j.log   # Standard output and error log
 #SBATCH --account=rrg-jacquese        #group account
 
@@ -19,9 +19,9 @@ pip install --no-index polars
 
 NXF_VER=25.04.2
 export NXF_OFFLINE=true
-/lustre06/project/6008022/All_user_common_folder/SOFTWARE/Nextflow/nextflow-25.04.2-dist run main.nf --cnvs /home/flben/projects/rrg-jacquese/flben/cnv_builder_work/data/test.tsv \
-    --genome_version "GRCh38" \
-    --cohort_tag "SPARK_XHMM" \
+/lustre06/project/6008022/All_user_common_folder/SOFTWARE/Nextflow/nextflow-25.04.2-dist run main.nf --cnvs $1 \
+    --genome_version "GRCh37" \
+    --cohort_tag "cnvDB_Buider" \
     -c setup/ccdb/ccdb.config \
-    -with-report results/SPARK_XHMM/docs/report.html \
+    -with-report results/SPARK_microarray/docs/report.html \
     -resume
