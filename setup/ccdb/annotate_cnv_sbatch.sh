5d72f326673a03b3f1e08149687a859a05f1330a
FloBeni <florian.benitiere@gmail.com>
2025-09-24 06:28:01 -0400

    update for use in SOFTWARE the setup CCDB

--- setup/ccdb/annotate_cnv_sbatch.sh
+++ setup/ccdb/annotate_cnv_sbatch.sh
@@ -31,24 +31,13 @@
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=3500MB                     # Job memory request
-#SBATCH --time=01:00:00               # Time limit hrs:min:sec
+#SBATCH --time=00:30:00               # Time limit hrs:min:sec
 #SBATCH --output=cnvDB_Buider_%j.log   # Standard output and error log
 #SBATCH --account=rrg-jacquese        #group account
 
 
-# Load modules
-module load java
-module load python/3.13.2
-module load nextflow
-export NXF_OFFLINE=true
-
-# Create and activate virtual environment
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index polars
-pip install --no-index duckdb
-
+# Default GIT_DIR
+git_dir="/home/flben/links/projects/rrg-jacquese/LAB_WORKSPACE/SOFTWARE/Git_pipeline/CNV-Annotation/"
 
 # ---------------------------
 # Argument parsing
@@ -58,7 +47,7 @@ while getopts "i:g:c:d:" opt; do
     i) cnv_input_file="$OPTARG" ;;
     g) genome_version="$OPTARG" ;;
     c) cohort_tag="$OPTARG" ;;
-    d) git_dir="$OPTARG" ;;
+    d) git_dir="$OPTARG" ;;  # overrides default if provided
     *) echo "Usage: $0 -i <CNV_TSV_FILE> -g <GENOME_VERSION> -c <COHORT_TAG> -d <GIT_DIR>"
        exit 1 ;;
   esac
@@ -72,8 +61,31 @@ if [ -z "$cnv_input_file" ] || [ -z "$genome_version" ] || [ -z "$cohort_tag" ] 
   exit 1
 fi
 
+# Check that main.nf exists in git_dir
+if [ ! -f "${git_dir}/main.nf" ]; then
+  echo "Error: Nextflow script '${git_dir}/main.nf' does not exist."
+  exit 1
+fi
+
 # Get script directory local
 # SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+# Load modules
+module load java
+module load python/3.13.2
+module load nextflow
+export NXF_OFFLINE=true
+
+# If SLURM_TMPDIR is not set, fallback to system temp dir
+TMPDIR=${SLURM_TMPDIR:-/tmp}
+
+# Create and activate virtual environment
+virtualenv --no-download "$TMPDIR/env"
+source "$TMPDIR/env/bin/activate"
+
+pip install --no-index --upgrade pip
+pip install --no-index polars
 
 # Run Nextflow pipeline
 nextflow run "${git_dir}/main.nf" --cnvs "$cnv_input_file" \
